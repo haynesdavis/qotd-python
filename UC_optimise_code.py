c95b07f00f05762d6c33a2e380d47bdb8175a6eb
haynesdavis <haynesdavis@gmail.com>
2024-11-16 10:16:52 -0800

    ss

--- UC_optimise_code.py
+++ UC_optimise_code.py
@@ -20,7 +20,6 @@ url = "http://9.46.241.25:9000/api/hotspots/search?inNewCodePeriod=false&onlyMin
 headers = {'Authorization': f'Basic {SONARQUBE_CREDS}'}
 
 sonarqube_response = requests.get(url, headers=headers)
-print(sonarqube_response.text)
 parsed_data = json.loads(sonarqube_response.text)
 
 with open('/tmp/code_optimisations.txt', 'w') as file:
