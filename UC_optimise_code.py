a7392d7b5a3e011515cf934db577a55ac34caf6f
haynesdavis <haynesdavis@gmail.com>
2024-11-16 05:11:42 -0800

    s

--- UC_optimise_code.py
+++ UC_optimise_code.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 import os
 import sys
+import requests
 
 genai.configure(api_key=os.environ["API_KEY"])
 
@@ -14,8 +15,13 @@ response = model.generate_content(f"Below given is a python code. Optimise the c
 print(response.text)
 
 SONARQUBE_CREDS = sys.argv[1]
-sonarqube_status=$(curl -H "Authorization: Basic $SONARQUBE_CREDS" "http://9.46.241.25:9000/api/project_branches/list?project=SmpleApp")
-print(sonarqube_status)
+
+url = "http://9.46.241.25:9000/api/project_branches/list?project=SmpleApp"
+headers = {'Authorization': f'Basic {SONARQUBE_CREDS}'}
+
+response = requests.get(url, headers=headers)
+print(response.text)
+
 
 with open('code_optimisations.txt', 'w') as file:
     file.write(response.text)
