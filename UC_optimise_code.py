b733e862eafe34adb1f27873738327449cde341e
haynesdavis <haynesdavis@gmail.com>
2024-11-16 06:02:18 -0800

    ss

--- UC_optimise_code.py
+++ UC_optimise_code.py
@@ -9,8 +9,8 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 with open('UC_code_to_scan.py', 'r') as file:
-    content = file.read()
-print(content)
+    original_code = file.read()
+
 genai_response = model.generate_content(f"Below given is a python code. Optimise the code.\n {content} \n Add this string at the begining of response - Optimised code is")
 
 SONARQUBE_CREDS = sys.argv[1]
@@ -21,6 +21,8 @@ headers = {'Authorization': f'Basic {SONARQUBE_CREDS}'}
 sonarqube_response = requests.get(url, headers=headers)
 
 with open('/tmp/code_optimisations.txt', 'w') as file:
+    file.write("Original code is + '\n')
+    file.write(original_code + '\n\n')
     file.write(genai_response.text + '\n\n')
     file.write("qualityGateStatus from SonarQube is as follows. Please take necessary action." + '\n')
     file.write(sonarqube_response.text)
